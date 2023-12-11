8a026d8841a457e662f2b620dccd660af43f4fc8
Nguyen Gia Bao <153185278+NGB-38@users.noreply.github.com>
2023-12-11 16:47:40 +0700

    Setting new Game Loop (set UPS)

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -5,6 +5,8 @@ public class Game implements Runnable {
     private GamePanel gamePanel;
     private Thread gameThread;
     private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
+
     public Game() {
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
@@ -17,30 +19,49 @@ public class Game implements Runnable {
         gameThread.start();
     }
 
+    public void update() {
+        gamePanel.updateGame();
+    }
+
     @Override //GAME LOOP
     public void run() {
         double timePerFrame = 1000000000 / FPS_SET;
-        long lastFrame = System.nanoTime();
-        long now = System.nanoTime(); //present time
+        double timePerUpdate = 1000000000 / UPS_SET;
+
+        long previousTime = System.nanoTime();
 
         int frames = 0;
+        int updates = 0;
         long lastCheck = System.currentTimeMillis();
 
+        double deltaU = 0;
+        double deltaF = 0;
 
         while (true) {
 
-            now = System.nanoTime();
-            if(now - lastFrame >= timePerFrame) {
+            long currentTime = System.nanoTime();
+            deltaU += (currentTime - previousTime)/timePerUpdate;
+            deltaF += (currentTime - previousTime)/timePerFrame;
+            previousTime = currentTime;
 
-                gamePanel.repaint();
-                lastFrame = now;
-                frames++;
+            if(deltaU >= 1){
+                update();
+                updates++;
+                deltaU--;
             }
+
+            if(deltaF >= 1){
+                gamePanel.repaint();
+                frames++;
+                deltaF--;
+            }
+
             //frames increment by 1 => repaint 1 frame until satisfy if condition
             if(System.currentTimeMillis() - lastCheck >= 1000) {
                 lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames);
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
                 frames = 0;
+                updates = 0;
             }
         }
 
